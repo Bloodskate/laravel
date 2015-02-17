cb151166e73a57dcac047419192a700c72cc311b
Bloodskate <prasannamishra50@gmail.com>
2015-02-17 15:22:38 +0545

    b

--- app/controllers/GroupController.php
+++ app/controllers/GroupController.php
@@ -1,11 +1,7 @@
 <?php
 class GroupController extends BaseController{
 	public function Apple(){
-		$posts = Status::where('primarygroup', '=', 'Apple')->get();
-		$users = User::where('user_id', '=', $posts->id);
-		return  View::make('group.apple')
-				->with('posts', $posts)
-				->with('users', $users);
+		return  View::make('group.apple');
 	}
 	public function Ball(){
 		return View::make('group.ball');
--- app/models/Status.php
+++ app/models/Status.php
@@ -2,4 +2,5 @@
 class Status extends Eloquent{
 	protected $table = 'posts';
 	protected $fillable = array('user_id', 'primarygroup', 'post');
+	
 }
